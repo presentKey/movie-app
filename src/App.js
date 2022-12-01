58f559c6421c05bf586c87ff79f18ae59e4f9821
presentKey <kim.hyeonjae4@gmail.com>
2022-12-02 03:00:18 +0900

    new: Set react Router and Create routes folder

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import { useEffect, useState } from 'react'
-import Movie from './Movie'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Detail from './routes/Detail'
+import Home from './routes/Home'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
-      )
-    ).json()
-
-    setMovies(json.data.movies)
-    setLoading(false)
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
