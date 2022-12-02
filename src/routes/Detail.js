63f1dffe410760ea1ed06133c9b682c7c28714d3
presentKey <kim.hyeonjae4@gmail.com>
2022-12-02 22:29:10 +0900

    new: Implement link to Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 function Detail() {
-  return <h1>Detail</h1>
+  const [loading, setLoading] = useState(true)
+  const [details, setDetails] = useState([])
+  const { id } = useParams()
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json()
+
+    setDetails(json.data.movie)
+    setLoading(false)
+  }
+  useEffect(() => {
+    getMovie()
+  }, [])
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h2>{details.title}</h2>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Detail
